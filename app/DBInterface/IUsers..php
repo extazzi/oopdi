6230ea62405c826e9285577be09a5a0555af1653
Ruslan Stshasliviy <extazzi2014@gmail.com>
2018-06-06 13:16:34 +0300

    Good 2.1

---
+++ app/DBInterface/IUsers..php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\DBInterface;
+
+interface IConnection
+{
+    public function setParams();
+}
---
+++ app/Traits/TUsers.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\traits;
+
+use App\DB\DBConnection;
+use App\DB\DBConfiguration;
+
+trait Connect
+{
+    public $conf;
+    public $db;
+    public function setParams(){
+        $this->conf = new DBConfiguration('root','','oop','localhost');
+        $this->db = new DBConnection($this->conf);
+    }
+}
